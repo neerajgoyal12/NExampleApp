49a83a4e6b3babd8ebd5b41acdc409543b33fb29
Neeraj Goyal <neerajgoyal12@gmail.com>
2017-11-13 23:58:58 +0530

    Initial Commit

---
+++ NExampleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NExampleApp
+//
+//  Created by Systology Software Pvt Ltd on 13/11/17.
+//  Copyright © 2017 Neeraj Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NExampleApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NExampleApp
+//
+//  Created by Systology Software Pvt Ltd on 13/11/17.
+//  Copyright © 2017 Neeraj Goyal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
